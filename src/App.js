15da854e2f0bf101e757eae00a01cf12531dc209
pandeiro13 <a51377@campus.fcsh.unl.pt>
2021-09-16 16:00:31 +0100

    primeiro

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p>o quê que é bom?</p>
+    </div>
+  );
+}
+
+export default App;
