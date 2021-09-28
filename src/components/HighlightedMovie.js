7fb86e539d0ac73117b7d6f7629773149f41f12b
pandeiro13 <a51377@campus.fcsh.unl.pt>
2021-09-28 11:55:58 +0100

    backgroung image in the higlighted section is done?

--- src/components/HighlightedMovie.js
+++ src/components/HighlightedMovie.js
@@ -3,8 +3,8 @@ import './Highlighted.css';
 
 export default ({item}) => { //this is the prop that we passed when we called this component in the App.js page
     return (
-        <section className='highlightedMovie'>
-            <div>{item.original_name}</div>
+        <section className='highlightedMovie' style={{backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`}}> {/*the style is how we will put the background image of the series in the highlighted section of the page. we don't know the size of the user's screen, so we don't want to gize a customized size to the image, we want the original size. this time, we don't want the poster, like we did in the movieGenre component. we will grab the backgroung image that comes from the API*/}
+            <div className='highlightedBackground'>{/* {item.original_name}*/}</div> 
         </section>
     )
 }
